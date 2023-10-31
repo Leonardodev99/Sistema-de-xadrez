ca9ec00cd89e0f284176da03508b475e7c598d24
Leonardodev99 <pequenotiago2016@gmail.com>
2023-10-31 05:48:03 +0000

    Criação da classe Position e da classe princial

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position ps = new Position(3, 5);
+		System.out.println(ps);
+
+	}
+
+}
